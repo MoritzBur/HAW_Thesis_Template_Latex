84bbe23b6d40c965804acb589e00b177dbb8021a
MoritzBur <moritz@burmester-iz.de>
2022-12-02 16:41:17 +0100

    initial commit functionally working wo errors

---
+++ allClean.ps1
@@ -0,0 +1,12 @@
+#allClean.ps1
+#reset directory to uncompiled initial state
+
+Remove-Item *.aux
+Remove-Item *.gz
+Remove-Item *.fls
+Remove-Item *.toc
+Remove-Item *.fdb_latexmk
+Remove-Item *.lof
+Remove-Item *.log
+Remove-Item *.xdv
+Remove-Item *.pdf
